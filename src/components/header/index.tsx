66e66d83b9ce5c365ceae200e94a12393a4b98f8
HelitonLeal <sleal.heliton@outlook.com>
2024-08-21 12:20:14 -0300

    Criação da base do Projeto com Navegação e Header inicial

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export function Header(){
   return(
     <header className={styles.container}>
-      <Link to={"/"}>
+      <Link to="/">
         <img src={logoImg} alt="Logo Cripto App" />
       </Link>
     </header>
