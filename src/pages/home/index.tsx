96dba1a0a9e80d8b21a9106e57d9fa41a32bcfda
HelitonLeal <sleal.heliton@outlook.com>
2024-08-21 14:36:21 -0300

    Criado a base da tela Home, com a devida customização

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,61 @@
 import styles from "./home.module.css";
+import { BsSearch } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
-export function Home(){
-  return(
-    <div>
-      <h1>Página Home!</h1>
-    </div>
+export function Home() {
+  return (
+    <main className={styles.container}>
+      <form className={styles.form}>
+        <input
+          type="text"
+          placeholder="Digite o nome da moeda..."
+        />
+
+        <button type="submit">
+          <BsSearch size={30} color="#fff" />
+        </button>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Moeda</th>
+            <th scope="col">Valor Mercado</th>
+            <th scope="col">Preço</th>
+            <th scope="col">Volume</th>
+            <th scope="col">Mudança 24Hs</th>
+          </tr>
+        </thead>
+
+        <tbody id="tbody">
+          <tr className={styles.tr}>
+            <td className={styles.tdLabel} data-label="Moeda">
+              <div className={styles.name}>
+                <Link to={"/datail/bitcoin"}>
+                  <span>Bitcoin</span> | BTC
+                </Link>
+              </div>
+            </td>
+
+            <td className={styles.tdLabel} data-label="Valor Mercado">
+              1T
+            </td>
+            
+            <td className={styles.tdLabel} data-label="Preço">
+              8.000
+            </td>
+
+            <td className={styles.tdLabel} data-label="Volume">
+              2B
+            </td>
+
+            <td className={styles.tdProfit} data-label="Mudança 24Hs">
+              <span>1.20</span>
+            </td>
+
+          </tr>
+        </tbody>
+      </table>
+    </main>
   );
 }
