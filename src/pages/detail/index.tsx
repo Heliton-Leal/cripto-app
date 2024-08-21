f389c0131593300ab6ef8320f1af649cd9bd5215
HelitonLeal <sleal.heliton@outlook.com>
2024-08-21 12:18:23 -0300

    Criação da base do Projeto com Navegação e Header inicial

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,9 @@
+
+
+export function Detail(){
+  return(
+    <div>
+      <h1>Página Detalhes!</h1>
+    </div>
+  );
+}
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,9 @@
+
+
+export function NotFound(){
+  return(
+    <div>
+      <h1>Erro 404</h1>
+    </div>
+  );
+}
